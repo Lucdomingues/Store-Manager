2bdd689e8aea3f882e9ac1199d8e571e1724d8bd
Lucdomingues <lojalucc@outlook.com>
2023-02-15 21:03:39 -0300

    [feat]requisito02-test-unit-completed

---
+++ tests/unit/controllers/productController.test.js
@@ -0,0 +1,50 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+
+const { expect } = chai;
+chai.use(sinonChai);
+
+const { productsServices } = require('../../../src/services/index');
+const { productsController } = require('../../../src/controllers/index');
+const { products } = require('./mock/product.controller.mock');
+
+describe('Testa a unidade do controller de products', function () {
+    it('Listando as pessoas passageiras e recebendo status 200', async function () {
+      const res = {};
+      const req = {};
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon
+        .stub(productsServices, 'findAll')
+        .resolves({ type: null, message: products });
+
+      await productsController.listPassengers(req, res);
+
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWith(products);
+    });
+  
+  it('Recuperando produtos pelo id e recebendo status 200', async function () {
+    const res = {};
+    const req = {
+      params: { id: 1 },
+    };
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+    sinon
+      .stub(productsServices, 'findById')
+      .resolves({ type: null, message: products[0] });
+
+    await productsController.listPassengersId(req, res);
+
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(products[0]);
+  });
+
+  afterEach(function () {
+    sinon.restore();
+  });
+});
